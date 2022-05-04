61ae3f2a8697a3ff093b177dda2fdc10bc12db69
Letícia Rose <50461464+leticiarose@users.noreply.github.com>
2022-05-04 01:10:11 -0300

    S01 – 04/05/2022 – Letícia – Criação de Funções Para Mudar Background ao Clicar

---
+++ src/JS/script.js
@@ -0,0 +1,9 @@
+function changeBgColor(color) {
+    const register = document.querySelector('.register');
+    register.style.background = color;
+}
+
+function changeBgColor(color) {
+    const login = document.querySelector('.login');
+    login.style.background = color;
+} 
