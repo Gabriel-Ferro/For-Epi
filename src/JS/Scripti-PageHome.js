a82434ee6b73923450181e19107a6609417be2fc
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2022-05-09 12:36:12 -0300

    S01 - 09/05/2022 - Cristhyam - Implementação de tela de login/Cadastro e implementação de tela de entrada.

---
+++ src/JS/Scripti-PageHome.js
@@ -0,0 +1,9 @@
+function changeBgColor(color) {
+    const register = document.querySelector('.administrador');
+    register.style.background = color;
+}
+
+function changeBgColor(color) {
+    const login = document.querySelector('.colaborador');
+    login.style.background = color;
+} 
--- src/JS/script.js
+++
@@ -1,9 +0,0 @@
-function changeBgColor(color) {
-    const register = document.querySelector('.register');
-    register.style.background = color;
-}
-
-function changeBgColor(color) {
-    const login = document.querySelector('.login');
-    login.style.background = color;
-} 
