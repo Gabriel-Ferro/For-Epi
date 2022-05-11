45f57c242e4f8b53680337f15586bcd03c632ad8
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2022-05-11 13:59:32 -0300

    S01 - 11/05/2022 - Cristhyam - Adicionando LocalStorage para a pagina de Cadastro do Colaborador do index.html.

--- src/JS/index.js
+++ src/JS/index.js
@@ -8,7 +8,19 @@ cadastro.addEventListener('click', function () {
         login.style.display = "block";
     }
 
-
-
 }
 )
+
+let salvarDados = function (){
+    let user = document.getElementById('user').value;
+    let CPF = document.getElementById('CPF').value;
+    localStorage.setItem('Nome do Colaborador:', user);
+    localStorage.setItem('CPF:', CPF);
+    let CelNumber = document.getElementById('Number-cel').value;
+    let ID = document.getElementById('ID').value;
+    localStorage.setItem('Número do Colaborador:', CelNumber);
+    localStorage.setItem('ID do Crachá:', ID);
+    let Cargo = document.getElementById('cargo').value;
+    localStorage.setItem('Cargo do Colaborador:', Cargo);
+  };
+  document.onchange = salvarDados;
