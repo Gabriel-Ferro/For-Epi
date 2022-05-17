685b92b1261728daca8fc97f48667abdf2f5993f
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2022-05-17 14:02:20 -0300

    S01 - 17/05/2022 - Cristhyam - Adicionado gravação e leitura do LocalStorage.

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,26 +1,45 @@
+var localStorage = Window.localStorage;
+var contador = 0;
+var objetos = new Array();
+
 const login = document.querySelector(".container_box");
-const cadastro = document.querySelector('.cadastro');
+const cadastro = document.querySelector(".cadastro");
 
-cadastro.addEventListener('click', function () {
+cadastro.addEventListener("click", function () {
+  cadastro.classList.toggle("active");
+  if (cadastro.classList.contains("active")) {
+    login.style.display = "block";
+  } else login.style.display = "none";
+});
 
-    cadastro.classList.add("active");
-    if (cadastro.classList.contains('active')) {
-        login.style.display = "block";
-    }
-
+function adiciona() {
+  var x = new Object();
+  x.user = document.getElementById("user").value;
+  x.cpf = document.getElementById("CPF").value;
+  x.celnumber = document.getElementById("Number-cel").value;
+  x.cracha = document.getElementById("ID").value;
+  x.cargo = document.getElementById("cargo").value;
+  objetos[contador] = x;
+  contador++;
+  console.log(objetos);
+  gravaDados();
 }
-)
+function gravaDados() {
+  var json = JSON.stringify(objetos);
+  localStorage.setItem("clientes", json);
+}
+function buscaDados() {
+  //Busca os dados na local storage e armazena em um vetor de objetos
 
-let salvarDados = function (){
-    let user = document.getElementById('user').value;
-    let CPF = document.getElementById('CPF').value;
-    localStorage.setItem('Nome do Colaborador:', user);
-    localStorage.setItem('CPF:', CPF);
-    let CelNumber = document.getElementById('Number-cel').value;
-    let ID = document.getElementById('ID').value;
-    localStorage.setItem('Número do Colaborador:', CelNumber);
-    localStorage.setItem('ID do Crachá:', ID);
-    let Cargo = document.getElementById('cargo').value;
-    localStorage.setItem('Cargo do Colaborador:', Cargo);
-  };
-  document.onchange = salvarDados;
+  var json = localStorage.getItem("clientes");
+
+  objetos = JSON.parse(json);
+  contador = objetos.length;
+  console.log(objetos[0].nome);
+}
+
+const form = document.getElementById("preventdefault");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Deu certo");
+});
