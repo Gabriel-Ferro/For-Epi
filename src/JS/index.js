fea19e2ff6687398ffca0e6b959515082fe3c965
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2022-05-24 23:58:42 -0300

    S01 - 24/05/2022 - Cristhyam - Formulário de cadastro de EPI.

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,15 +1,41 @@
 var localStorage = Window.localStorage;
 var contador = 0;
 var objetos = new Array();
+var json = localStorage.getItem("colaborador");
+objetos = JSON.parse(json);
+contador = objetos.length;
 
 const login = document.querySelector(".container_box");
+const mainColab = document.querySelector(".mainColab");
+const loginEPI = document.querySelector(".Container_BOXEPI");
+const mainEPI = document.querySelector(".mainEPI");
 const cadastro = document.querySelector(".cadastro");
+const cadastroEPI = document.querySelector(".cadastroEPI");
 
 cadastro.addEventListener("click", function () {
   cadastro.classList.toggle("active");
   if (cadastro.classList.contains("active")) {
+    mainColab.style.display = "block";
     login.style.display = "block";
-  } else login.style.display = "none";
+    loginEPI.style.display = "none";
+    mainEPI.style.display = "none";
+  } else {
+    login.style.display = "none";
+    mainColab.style.display = "none";
+  }
+});
+
+cadastroEPI.addEventListener("click", function () {
+  cadastroEPI.classList.toggle("active");
+  if (cadastroEPI.classList.contains("active")) {
+    mainEPI.style.display = "block";
+    loginEPI.style.display = "block";
+    login.style.display = "none";
+    mainColab.style.display = "none";
+  } else {
+    loginEPI.style.display = "none";
+    mainEPI.style.display = "none";
+  }
 });
 
 function adiciona() {
@@ -23,16 +49,20 @@ function adiciona() {
   contador++;
   console.log(objetos);
   gravaDados();
+  document.getElementById("user").value = "";
+  document.getElementById("CPF").value = "";
+  document.getElementById("Number-cel").value = "";
+  document.getElementById("ID").value = "";
+  document.getElementById("cargo").value = "";
+  document.getElementById("user").focus();
 }
 function gravaDados() {
   var json = JSON.stringify(objetos);
-  localStorage.setItem("clientes", json);
+  localStorage.setItem("colaborador", json);
 }
 function buscaDados() {
   //Busca os dados na local storage e armazena em um vetor de objetos
-
-  var json = localStorage.getItem("clientes");
-
+  var json = localStorage.getItem("colaborador");
   objetos = JSON.parse(json);
   contador = objetos.length;
   console.log(objetos[0].nome);
